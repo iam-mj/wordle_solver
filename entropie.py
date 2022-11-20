aa1cad793a7ff80679ce3f0e1e56ae1e145302f7
mj_v <mariaverdes14@gamil.com>
2022-11-20 21:00:06 +0200

    tried to fix the probabilities, but failed miserably

--- entropie.py
+++ entropie.py
@@ -9,70 +9,99 @@ LIT = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 def gri(lit, poz):
     return 1 - galben(lit, poz) - verde(lit, poz)
 
-def galben(lit, poz):
+def gri2(lit, poz, poz2 = -1, poz3 = -1):
     s = 0
     for i in range(5):
-        if i != poz:
-            s += P[lit][i]
-    return s / N 
+        if i != poz and i != poz2 and i != poz3:
+            s += N - P[lit][i]
+    return s / N
 
+def gri3(lit, poz1, poz2):
+    s = 0
+    s += N - P[lit][poz1] + N - P[lit][poz2]
+    return s / N
+
+def galben(lit, poz, poz2 = -1, poz3 = -1, poz4 = -1):
+    s = 0
+    for i in range(5):
+        if i != poz and i != poz2 and i != poz3 and i != poz4:
+            s += P[lit][i]
+    return s / N * (1 - P[lit][poz] / N)
+
+"""
 def galben2(lit, poz1, poz2):
     s = 0
     for i in range(5):
         if i != poz1 and i != poz2:
             s += P[lit][i]
-    return s / N 
+    return s / N #* (1 - P[lit][poz1] / N - P[lit][poz2] / N)
+"""
 
 def verde(lit, poz):
     return P[lit][poz] / N
 
 def entropie(cuvant):
+
     Dict = {k:[] for k in cuvant}
     for i in range(len(cuvant)):
         Dict[cuvant[i]].append(i)
     ENTROPIE = 0
+
     #toate numerele de la 0 la 243 in baza 3
     cnt = 0
     while cnt < 243:
+
         #transformam numarul in baza 3
         i = cnt
-        LIST50 = [0 for i in range(5)]
+        LIST = [0 for i in range(5)]
         nr = 1 #contorizam impartirile
         while i > 0:
             r = i % 3 # 0 - gri, 1 - galben, 2 - verde
-            LIST50[5 - nr] = r
+            LIST[5 - nr] = r
             i = i // 3
             nr += 1
         cnt += 1
+
         #calculam cata informatie ne ofera fiecare posibil feedback
         p = 1
-        for i in range(len(LIST50)):
+        for i in range(len(LIST)):
+
             if len(Dict[cuvant[i]]) == 2:
+
                 ind = i 
                 for j in Dict[cuvant[i]]: 
                     ind = ind ^ j #cealalta pozitie pe care se mai gaseste litera in cuvant
-                if (LIST50[ind] == 1 or LIST50[ind] == 2) and LIST50[i] == 0:
-                    p *= 1 - P[cuvant[i]][i] / N
-                elif (LIST50[i] == 1 or LIST50[i] == 2) and LIST50[ind] == 0:
-                    if LIST50[i] == 1:
-                        p *= galben(cuvant[i], i)
+
+                if (LIST[ind] == 1 or LIST[ind] == 2) and LIST[i] == 0:
+                    if LIST[ind] == 2:
+                        p *= gri2(cuvant[i], ind)
+                    else:
+                        p *= gri3(cuvant[i], i, ind)
+
+                elif (LIST[i] == 1 or LIST[i] == 2) and LIST[ind] == 0:
+                    if LIST[i] == 1:
+                        p *= galben(cuvant[i], i, ind)
                     else: 
                         p *= verde(cuvant[i], i)
-                elif LIST50[ind] == LIST50[i]:
-                    if LIST50[i] == 0 and i < ind:
+
+                elif LIST[ind] == LIST[i]:
+                    if LIST[i] == 0 and i < ind:
                         p *= gri(cuvant[i], i)
-                    elif LIST50[i] == 1 and i < ind:
-                        p *= galben2(cuvant[i], i, ind) 
+                    elif LIST[i] == 1 and i < ind:
+                        p *= galben(cuvant[i], i, ind) 
                     else:
                         p *= verde(cuvant[i], i)
-                elif LIST50[i] == 2 and LIST50[ind] == 1:
+
+                elif LIST[i] == 2 and LIST[ind] == 1:
                     p *= verde(cuvant[i], i)
+
                 else:
-                    p *= galben2(cuvant[i], i, ind) 
+                    p *= galben(cuvant[i], i, ind) 
+
             else:
-                if LIST50[i] == 0:
+                if LIST[i] == 0:
                         p *= gri(cuvant[i], i)
-                elif LIST50[i] == 1:
+                elif LIST[i] == 1:
                     p *= galben(cuvant[i], i)
                 else: 
                     p *= verde(cuvant[i], i)
@@ -98,8 +127,6 @@ for i in range(len(L)):
     for j in range(5):
         P[L[i][j]][j] += 1
 
-print(entropie("TAREI"))
-"""
 emax = 0
 cuvmax = ""
 for i in L:
@@ -110,4 +137,3 @@ for i in L:
 
 print(cuvmax)
 print(emax)
-"""
