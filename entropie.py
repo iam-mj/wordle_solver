aae2e0b2470f0920aaa8fef3f1d05df2217d0ebd
mj_v <mariaverdes14@gamil.com>
2022-11-22 15:09:35 +0200

    did my best with the entropy

--- entropie.py
+++ entropie.py
@@ -6,36 +6,25 @@ L = []
 N = 11454 #numarul de cuvinte din baza de date
 LIT = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-def gri(lit, poz):
-    return 1 - galben(lit, poz) - verde(lit, poz)
-
-def gri2(lit, poz, poz2 = -1, poz3 = -1):
+def gri(lit):
     s = 0
     for i in range(5):
-        if i != poz and i != poz2 and i != poz3:
-            s += N - P[lit][i]
-    return s / N
+        s += N - P[lit][i]
+    return s / (5 * N)
 
-def gri3(lit, poz1, poz2):
-    s = 0
-    s += N - P[lit][poz1] + N - P[lit][poz2]
-    return s / N
-
-def galben(lit, poz, poz2 = -1, poz3 = -1, poz4 = -1):
+def galben(lit, poz):
     s = 0
     for i in range(5):
-        if i != poz and i != poz2 and i != poz3 and i != poz4:
+        if i != poz:
             s += P[lit][i]
-    return s / N * (1 - P[lit][poz] / N)
+    return s / (5 * N)
 
-"""
 def galben2(lit, poz1, poz2):
     s = 0
     for i in range(5):
         if i != poz1 and i != poz2:
             s += P[lit][i]
-    return s / N #* (1 - P[lit][poz1] / N - P[lit][poz2] / N)
-"""
+    return s / (5 * N)
 
 def verde(lit, poz):
     return P[lit][poz] / N
@@ -66,46 +55,38 @@ def entropie(cuvant):
         p = 1
         for i in range(len(LIST)):
 
+            #cazurile pentru 2 litere identice
             if len(Dict[cuvant[i]]) == 2:
 
                 ind = i 
                 for j in Dict[cuvant[i]]: 
                     ind = ind ^ j #cealalta pozitie pe care se mai gaseste litera in cuvant
 
-                if (LIST[ind] == 1 or LIST[ind] == 2) and LIST[i] == 0:
-                    if LIST[ind] == 2:
-                        p *= gri2(cuvant[i], ind)
-                    else:
-                        p *= gri3(cuvant[i], i, ind)
-
-                elif (LIST[i] == 1 or LIST[i] == 2) and LIST[ind] == 0:
-                    if LIST[i] == 1:
-                        p *= galben(cuvant[i], i, ind)
-                    else: 
-                        p *= verde(cuvant[i], i)
-
-                elif LIST[ind] == LIST[i]:
-                    if LIST[i] == 0 and i < ind:
-                        p *= gri(cuvant[i], i)
-                    elif LIST[i] == 1 and i < ind:
-                        p *= galben(cuvant[i], i, ind) 
-                    else:
-                        p *= verde(cuvant[i], i)
-
+                if LIST[i] == 0 and LIST[ind] == 0 and i < ind:
+                    p *= gri(cuvant[i])
+                elif ((LIST[i] == 0 and LIST[ind] == 1) or (LIST[i] == 1 and LIST[ind] == 0)) and i < ind:
+                    p *= galben2(cuvant[i], i, ind)
+                elif LIST[i] == 2 and LIST[ind] == 0:
+                    p *= verde(cuvant[i], i)
+                elif LIST[i] == 1 and LIST[ind] == 1:
+                    p *= galben2(cuvant[i], i, ind)
+                elif LIST[i] == 1 and LIST[ind] == 2:
+                    p *= galben2(cuvant[i], i, ind)
                 elif LIST[i] == 2 and LIST[ind] == 1:
                     p *= verde(cuvant[i], i)
-
-                else:
-                    p *= galben(cuvant[i], i, ind) 
+                elif LIST[i] == 2 and LIST[ind] == 2:
+                    p *= verde(cuvant[i], i)
 
             else:
                 if LIST[i] == 0:
-                        p *= gri(cuvant[i], i)
+                    p *= gri(cuvant[i])
                 elif LIST[i] == 1:
                     p *= galben(cuvant[i], i)
                 else: 
                     p *= verde(cuvant[i], i)
+
         ENTROPIE += p * (math.log2(1 / p))
+
     return ENTROPIE
         
 
@@ -120,9 +101,8 @@ cuv = L[n]
 print(cuv)
 
 
-#calculam probabilitatea de aparitie a fiecarei litere
+#calculam probabilitatea de aparitie a fiecarei litere pe fiecare pozitie
 P = {k:[0, 0, 0, 0, 0] for k in LIT}
-num = 5 * N #numarul total de litere din fisier
 for i in range(len(L)):
     for j in range(5):
         P[L[i][j]][j] += 1
@@ -134,6 +114,5 @@ for i in L:
     if e > emax:
         emax = e
         cuvmax = i
-
 print(cuvmax)
 print(emax)
